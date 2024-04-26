12a6730ad26cd59c4125ac747e0def2ca8e92e17
austinb847 <austinb847@icloud.com>
2024-04-26 12:34:34 -0700

    chore: config tailwind themes and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#000000',
+        teal: '#27A98B',
+        offwhite: '#F2EEEE'
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
